b6dcbdd818a6ccb1b07d45afe9d0f45dcbf03f2b
sanggayasa <sanggayasa502@gmail.com>
2021-09-26 15:59:18 +0700

    perbaikan cek login

--- index.php
+++ index.php
@@ -23,7 +23,7 @@
 if (isset($_COOKIE['id']) && isset($_COOKIE['key'])) {
   $id  = $_COOKIE['id'];
   $key = $_COOKIE['key'];
-  include 'koneksi2.php';
+  include 'connection.php';
   $query = mysql_query("SELECT username FROM admin WHERE username = '$id' ");
   $row = mysql_fetch_assoc($query);
   if (hash('sha384', $row['username']) == $key) {
@@ -78,8 +78,10 @@ if (isset($_GET['pesan'])) {
                   <div class="text-center">
                     <h1 class="h4 text-gray-900 mb-4">Welcome Back!</h1>
                   </div>
-                  <form class="user" action="cek_login.php" method="post">
-                    <!-- <div class="form-group">
+
+                  <!-- 
+                      <form class="user" action="cek_login.php" method="post">
+                        <div class="form-group">
                       <input type="email" class="form-control form-control-user" id="exampleInputEmail" aria-describedby="emailHelp" placeholder="Enter Email Address...">
                     </div>
                     <div class="form-group">
@@ -101,22 +103,22 @@ if (isset($_GET['pesan'])) {
                     <a href="index.html" class="btn btn-facebook btn-user btn-block">
                       <i class="fab fa-facebook-f fa-fw"></i> Login with Facebook
                     </a> -->
-                    <form class="user" action="controller/login.php" method="post">
-                      <div class="form-group">
-                        <input type="text" name="username" class="form-control form-control-user" maxlength="15" id="exampleInputEmail" aria-describedby="emailHelp" placeholder="Enter Username..." required>
+                  <form class="user" action="controller/login.php" method="post">
+                    <div class="form-group">
+                      <input type="text" name="username" class="form-control form-control-user" maxlength="15" id="exampleInputEmail" aria-describedby="emailHelp" placeholder="Enter Username..." required>
+                    </div>
+                    <div class="form-group">
+                      <input type="password" name="password" class="form-control form-control-user" id="exampleInputPassword" placeholder="Password" maxlength="15" required>
+                    </div>
+                    <div class="form-group">
+                      <div class="custom-control custom-checkbox small">
+                        <input type="checkbox" class="custom-control-input" id="customCheck" name="rememberme">
+                        <label class="custom-control-label" for="customCheck">Remember Me</label>
                       </div>
-                      <div class="form-group">
-                        <input type="password" name="password" class="form-control form-control-user" id="exampleInputPassword" placeholder="Password" maxlength="15" required>
-                      </div>
-                      <div class="form-group">
-                        <div class="custom-control custom-checkbox small">
-                          <input type="checkbox" class="custom-control-input" id="customCheck" name="rememberme">
-                          <label class="custom-control-label" for="customCheck">Remember Me</label>
-                        </div>
-                      </div>
-                      <input type="submit" name="login" value="login" class="btn btn-primary btn-user btn-block">
-                      <hr>
-                    </form>
+                    </div>
+                    <input type="submit" name="login" value="login" class="btn btn-primary btn-user btn-block">
+                    <hr>
+                  </form>
                   </form>
                   <hr>
                   <div class="text-center">
